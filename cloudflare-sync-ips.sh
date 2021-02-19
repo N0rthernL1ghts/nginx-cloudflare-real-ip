d21f3596915f6a1137d180dbebd5a132e30060fb
xZero707 <xzero@elite7hackers.net>
2021-02-19 22:36:07 +0100

    Improve realibility by not modifying config directly

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 NGINX_CF_FILE="${NGINX_CF_FILE:-/etc/nginx/cloudflare}"
+VERBOSE_MODE=${VERBOSE_MODE:-true}
 
 # Data endpoints
 CF_IPV4_ENDPOINT="${CF_IPV4_ENDPOINT:-https://www.cloudflare.com/ips-v4/}"
@@ -8,27 +9,40 @@ CF_IPV6_ENDPOINT="${CF_IPV6_ENDPOINT:-https://www.cloudflare.com/ips-v6/}"
 
 . ./src/functions.sh
 
-echo "#Cloudflare" >"${NGINX_CF_FILE}"
-echo "" >>"${NGINX_CF_FILE}"
+trap 'rm -f "${TMP_FILE}"' EXIT
+TMP_FILE="$(mktemp)"
 
-echo "# - IPv4" >>"${NGINX_CF_FILE}"
+if [ -z "${TMP_FILE}" ]; then
+  echo "Error creating temporary file"
+  exit 1
+fi
+
+echo "#Cloudflare" >"${TMP_FILE}"
+echo "" >>"${TMP_FILE}"
+
+echo "# - IPv4" >>"${TMP_FILE}"
 
 CF_IPV4_RANGES=$(fetchCloudFlareIPRanges "${CF_IPV4_ENDPOINT}")
 if [ -n "${CF_IPV4_RANGES}" ]; then
   for IP_RANGE in ${CF_IPV4_RANGES}; do
-    echo "set_real_ip_from ${IP_RANGE};" >>"${NGINX_CF_FILE}"
+    echo "set_real_ip_from ${IP_RANGE};" >>"${TMP_FILE}"
   done
 fi
 
-echo "" >>"${NGINX_CF_FILE}"
-echo "# - IPv6" >>"${NGINX_CF_FILE}"
+echo "" >>"${TMP_FILE}"
+echo "# - IPv6" >>"${TMP_FILE}"
 
 CF_IPV6_RANGES=$(fetchCloudFlareIPRanges "${CF_IPV6_ENDPOINT}")
 if [ -n "${CF_IPV6_RANGES}" ]; then
   for IP_RANGE in ${CF_IPV6_RANGES}; do
-    echo "set_real_ip_from ${IP_RANGE};" >>"${NGINX_CF_FILE}"
+    echo "set_real_ip_from ${IP_RANGE};" >>"${TMP_FILE}"
   done
 fi
 
-echo "" >>"${NGINX_CF_FILE}"
-echo "real_ip_header CF-Connecting-IP;" >>"${NGINX_CF_FILE}"
+echo "" >>"${TMP_FILE}"
+echo "real_ip_header CF-Connecting-IP;" >>"${TMP_FILE}"
+
+if validateNginxConfig "${TMP_FILE}"; then
+  echo "Flushing file"
+  cat "${TMP_FILE}" >"${NGINX_CF_FILE}"
+fi
--- src/functions.sh
+++ src/functions.sh
@@ -1,20 +1,70 @@
 #!/usr/bin/env sh
 
+println() {
+  if [ "${VERBOSE_MODE:-}" = "true" ]; then
+    echo "${1:-}"
+    return 0
+  fi
+  return 1
+}
+
+printlnError() {
+  if [ "${VERBOSE_MODE:-}" = "true" ]; then
+    echo >&2 "${1:-}"
+    return 0
+  fi
+  return 1
+}
+
 fetchCloudFlareIPRanges() {
   ENDPOINT="${1:-}"
 
   if [ -z "${ENDPOINT}" ]; then
-    echo >&2 "Argument error:  Missing endpoint"
+    printlnError "Argument error:  Missing endpoint"
     return 1
   fi
 
-  CF_IP_RANGES=$(curl --silent "${ENDPOINT}")
+  CF_IP_RANGES=$(wget --content-on-error -qO- "${ENDPOINT}")
+
+  if [ "${?}" != 0 ]; then
+    printlnError "Error: Fetching IP ranges from '${ENDPOINT}' failed. Invalid response"
+    printlnError "Warning: Skipping section"
+    return 1
+  fi
 
   if [ -n "${CF_IP_RANGES}" ]; then
     echo "${CF_IP_RANGES}"
     return 0
   fi
 
-  echo >&2 "Error occurred fetching IP ranges from ${ENDPOINT}"
+  printlnError "Error: Fetching IP ranges from '${ENDPOINT}' failed. Empty response"
   return 1
+}
+
+isDockerContainer() {
+  if [ -f /.dockerenv ]; then
+    return 0
+  fi
+  return 1
+}
+
+validateNginxConfig() {
+  CONF_FILE="${1:-}"
+
+  if [ -z "${CONF_FILE}" ]; then
+    printlnError "Argument error:  Missing endpoint"
+    return 1
+  fi
+
+  if [ ! -f "${CONF_FILE}" ]; then
+    printlnError "validateNginxConfig: ${CONF_FILE}: No such file or directory"
+    return 1
+  fi
+
+  if isDockerContainer; then
+    nginx -t -c "$(realpath ${CONF_FILE})"
+    return $?
+  fi
+
+  printlnError "Warning: Not validated with nginx. Docker environment unavailable"
 }
