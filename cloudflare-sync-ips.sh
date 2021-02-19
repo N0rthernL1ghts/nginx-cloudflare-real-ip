4887780f83c87a1f9f25faaf2eeea8ea30144c4d
xZero707 <xzero@elite7hackers.net>
2021-02-19 19:12:02 +0100

    Apply formatting

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -8,27 +8,27 @@ CF_IPV6_ENDPOINT="${CF_IPV6_ENDPOINT:-https://www.cloudflare.com/ips-v6}"
 
 . ./src/functions.sh
 
-echo "#Cloudflare" > "${NGINX_CF_FILE}";
-echo "" >> "${NGINX_CF_FILE}";
+echo "#Cloudflare" >"${NGINX_CF_FILE}"
+echo "" >>"${NGINX_CF_FILE}"
 
-echo "# - IPv4" >> "${NGINX_CF_FILE}";
+echo "# - IPv4" >>"${NGINX_CF_FILE}"
 
 CF_IPV4_RANGES=$(fetchCloudFlareIPRanges "${CF_IPV4_ENDPOINT}")
 if [ -n "${CF_IPV4_RANGES}" ]; then
   for IP_RANGE in ${CF_IPV4_RANGES}; do
-    echo "set_real_ip_from ${IP_RANGE};" >> "${NGINX_CF_FILE}";
+    echo "set_real_ip_from ${IP_RANGE};" >>"${NGINX_CF_FILE}"
   done
 fi
 
-echo "" >> "${NGINX_CF_FILE}";
-echo "# - IPv6" >> "${NGINX_CF_FILE}";
+echo "" >>"${NGINX_CF_FILE}"
+echo "# - IPv6" >>"${NGINX_CF_FILE}"
 
 CF_IPV6_RANGES=$(fetchCloudFlareIPRanges "${CF_IPV6_ENDPOINT}")
 if [ -n "${CF_IPV6_RANGES}" ]; then
   for IP_RANGE in ${CF_IPV6_RANGES}; do
-    echo "set_real_ip_from ${IP_RANGE};" >> "${NGINX_CF_FILE}";
+    echo "set_real_ip_from ${IP_RANGE};" >>"${NGINX_CF_FILE}"
   done
 fi
 
-echo "" >> "${NGINX_CF_FILE}";
-echo "real_ip_header CF-Connecting-IP;" >> "${NGINX_CF_FILE}";
+echo "" >>"${NGINX_CF_FILE}"
+echo "real_ip_header CF-Connecting-IP;" >>"${NGINX_CF_FILE}"
--- src/functions.sh
+++ src/functions.sh
@@ -4,7 +4,7 @@ fetchCloudFlareIPRanges() {
   ENDPOINT="${1:-}"
 
   if [ -z "${ENDPOINT}" ]; then
-    >&2 echo "Argument error:  Missing endpoint"
+    echo >&2 "Argument error:  Missing endpoint"
     return 1
   fi
 
@@ -15,6 +15,6 @@ fetchCloudFlareIPRanges() {
     return 0
   fi
 
-  >&2 echo "Error occurred fetching IP ranges from ${ENDPOINT}"
+  echo >&2 "Error occurred fetching IP ranges from ${ENDPOINT}"
   return 1
 }
